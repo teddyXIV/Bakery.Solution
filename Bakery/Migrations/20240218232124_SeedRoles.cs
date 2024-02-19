56348e52489bf95f98498578a7e8b5edfdf6a5b1
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-02-18 16:07:18 -0800

    WIP adding roles

---
+++ Bakery/Migrations/20240218232124_SeedRoles.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bakery.Migrations
+{
+    public partial class SeedRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "1", "e5a82591-7abd-4f7f-84c8-27e666280e80", "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "2", "09cd5212-0a30-456e-872f-6d05c5c59dac", "User", "USER" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2");
+        }
+    }
+}
